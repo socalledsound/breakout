8c14e02410a4cd9807b77274fa9c3ecfd202b4e2
ck <ck@cks-macbook-pro.lan>
2022-09-06 20:15:19 -0700

    block class

---
+++ Block.js
@@ -0,0 +1,30 @@
+class Block {
+    constructor(x, y, col){
+        this.x = x
+        this.y = y
+        this.w = blockWidth
+        this.h = blockHeight
+        this.col = col
+        this.strokeCol = 'blue'
+        this.gone = false
+    }
+
+    checkBall(ball){
+        if(!this.gone){
+            if(checkCircleRectCollision(ball.pos.x, ball.pos.y, ball.r, this.x, this.y, this.w, this.h)){
+                this.gone = true
+                ball.vel.y *= -1
+                score ++
+            }
+        }
+
+    }
+
+    render(){
+        if(!this.gone){
+            stroke(this.strokeCol)
+            fill(this.col)
+            rect(this.x, this.y, this.w, this.h)
+        }
+    }
+}
--- sketch.js
+++ sketch.js
@@ -1,22 +1,64 @@
+const canvasWidth = 800
+const canvasHeight = 800
+const paddingLeft = 25
+const paddingTop = 100
+const blockPadding = 10
+const blockWidth = 40
+const blockHeight = 20
+const numCols = 15
+const numRows = 6
+const rowColors = [
+    'brown',
+    'pink',
+    'green',
+    'orange',
+    'yellow',
+    'purple',
+]
+
 let ball, paddle
 let gameOver = false
+let score = 0
+let blocks = []
+
+
+for(let i = 0; i < numRows; i++){
+    blocks[i] = new Array(numCols)
+}
+
+console.log(blocks)
 
 
 function setup(){
-    createCanvas(800, 800)
-    ball = new Ball()
-    paddle = new Paddle()
+    createCanvas(canvasWidth, canvasHeight)
+    initGame()
+    
 }
 
 function draw(){
+
+    
     if(!gameOver){
         background(200)
+        textSize(30)
+        stroke(0)
+        text('score: ' + score, 20, 40)
         ball.update()
         ball.render()
     
         paddle.render()
         paddle.checkBall(ball)
     
+        blocks.forEach(row => {
+            row.forEach(block => {
+                //console.log(block)
+                block.checkBall(ball)
+                block.render()
+            })
+        })
+
+
+
         if(keyIsPressed){
             if(keyCode === LEFT_ARROW){
                     if(paddle.x > 0){
@@ -37,6 +79,31 @@ function draw(){
     }
 
 }
+
+function initGame(){
+    score = 0
+    ball = new Ball()
+    paddle = new Paddle()
+
+    for(let y = 0; y < numRows; y++){
+        for(let x = 0; x < numCols; x ++){
+            blocks[y][x] = new Block(x * blockWidth + paddingLeft + blockPadding * x, 
+                                        y * blockHeight + paddingTop + blockPadding * y,
+                                        rowColors[y])
+        }
+    }
+    console.log(blocks)
+}
+
+
+function mousePressed(){
+    if(gameOver){
+        gameOver = false
+        initGame()
+    }
+}
+
+
 
 // function keyPressed(){
 //     if(keyCode === LEFT_ARROW){
